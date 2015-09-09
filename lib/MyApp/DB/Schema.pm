1aef32df818af4cb3a67a36092bfa64829e4d494
root <root@web0001.(none)>
2015-09-09 17:51:25 +0900

    mcMMO及びblock_id_name テーブルを追加

--- lib/MyApp/DB/Schema.pm
+++ lib/MyApp/DB/Schema.pm
@@ -1,7 +1,16 @@
-package MyApp::DB::Shema;
+package MyApp::DB::Shema::Schema;
 use strict;
 use warnings;
 use Teng::Schema::Declare;
+table {
+    name 'block_id_name';
+    pk 'id';
+    columns (
+        {name => 'name', type => 12},
+        {name => 'id', type => 12},
+    );
+};
+
 table {
     name 'hawk_players';
     pk 'player_id';
@@ -33,6 +42,87 @@ table {
         {name => 'player_id', type => 4},
         {name => 'action', type => 4},
         {name => 'z', type => 4},
+    );
+};
+
+table {
+    name 'mcmmo_cooldowns';
+    pk 'user_id';
+    columns (
+        {name => 'blast_mining', type => 4},
+        {name => 'axes', type => 4},
+        {name => 'repair', type => 4},
+        {name => 'mining', type => 4},
+        {name => 'archery', type => 4},
+        {name => 'herbalism', type => 4},
+        {name => 'woodcutting', type => 4},
+        {name => 'taming', type => 4},
+        {name => 'acrobatics', type => 4},
+        {name => 'excavation', type => 4},
+        {name => 'unarmed', type => 4},
+        {name => 'user_id', type => 4},
+        {name => 'swords', type => 4},
+    );
+};
+
+table {
+    name 'mcmmo_experience';
+    pk 'user_id';
+    columns (
+        {name => 'alchemy', type => 4},
+        {name => 'axes', type => 4},
+        {name => 'repair', type => 4},
+        {name => 'mining', type => 4},
+        {name => 'archery', type => 4},
+        {name => 'herbalism', type => 4},
+        {name => 'woodcutting', type => 4},
+        {name => 'taming', type => 4},
+        {name => 'fishing', type => 4},
+        {name => 'acrobatics', type => 4},
+        {name => 'excavation', type => 4},
+        {name => 'unarmed', type => 4},
+        {name => 'user_id', type => 4},
+        {name => 'swords', type => 4},
+    );
+};
+
+table {
+    name 'mcmmo_huds';
+    pk 'user_id';
+    columns (
+        {name => 'mobhealthbar', type => 12},
+        {name => 'user_id', type => 4},
+    );
+};
+
+table {
+    name 'mcmmo_skills';
+    pk 'user_id';
+    columns (
+        {name => 'alchemy', type => 4},
+        {name => 'axes', type => 4},
+        {name => 'repair', type => 4},
+        {name => 'mining', type => 4},
+        {name => 'archery', type => 4},
+        {name => 'herbalism', type => 4},
+        {name => 'woodcutting', type => 4},
+        {name => 'taming', type => 4},
+        {name => 'fishing', type => 4},
+        {name => 'acrobatics', type => 4},
+        {name => 'excavation', type => 4},
+        {name => 'unarmed', type => 4},
+        {name => 'user_id', type => 4},
+        {name => 'swords', type => 4},
+    );
+};
+
+table {
+    name 'mcmmo_users';
+    pk 'id';
+    columns (
+        {name => 'user', type => 12},
+        {name => 'id', type => 4},
+        {name => 'lastlogin', type => 4},
     );
 };
 
--- script/schema_dumper.pl
+++ script/schema_dumper.pl
@@ -2,7 +2,6 @@
 use strict;
 use warnings;
 use utf8;
-use Test::mysqld;
 use DBI;
 use Teng::Schema::Dumper;
 use Path::Class qw/file/;
@@ -17,23 +16,11 @@ my $mysqld = Test::mysqld->new(
     }
 ) or die $Test::mysqld::errstr;
 
-my $dbh = DBI->connect(
-    $mysqld->dsn.';mysql_multi_statements=1',
-) or die "can't create dbh";
-
-$dbh->do('create database test_db');
-my $sql = "use test_db;\n";
-$sql   .= "set names utf8;\n";
-$sql   .= file($mysql_schema_path)->slurp(iomode => '<:encoding(UTF-8)');
-$dbh->do($sql);
+my $dbh = DBI->connect("dbi:mysql:dbname=minecraft", 'root', 'root', +{mysql_enable_utf8 => 1}) or die "can't create dbh";
 
 my $schema = Teng::Schema::Dumper->dump(
     dbh       => $dbh,
     namespace => $teng_schema_class,
-#    inflate   => $inflate_conf, #inlateを入れたい時はここにほげほげ
 );
-
-## なんか入れ込みたい時は、ここで強引に置換
-#$schema =~ s!Teng\:\:Schema\:\:Declare\;!Teng\:\:Schema\:\:Declare\;\nuse Time::Piece\;!;
 
 print $schema;
